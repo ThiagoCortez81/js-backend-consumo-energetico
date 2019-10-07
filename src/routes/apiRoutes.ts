114b8efb8a71c16ce9531910da55ff7dd76f212b
Thiago Cortez <thiago.cortez81@gmail.com>
2019-10-07 20:39:06 -0300

    adicionando cache para melhorar o desempenho

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -3,6 +3,7 @@ import userController from "../controller/userController";
 import sensorController from "../controller/sensorController";
 import {Utils} from "./utils";
 import dadosMedicoesController from "../controller/dadosMedicoesController";
+import * as apicache from 'apicache'
 
 class ApiRoutes {
 
@@ -13,6 +14,8 @@ class ApiRoutes {
     }
 
     config(): void {
+        let cache = apicache.middleware;
+
         this.router.get('/', (req, res) => {
             res.send({"message": "It works!"})
         });
@@ -20,11 +23,11 @@ class ApiRoutes {
         this.router.post('/login', userController.loginUsuario);
         this.router.post('/cadastrarUsuario', userController.cadastrarUsuario);
 
-        this.router.post('/listarSensoresCliente', Utils.verifyJWT, sensorController.listarSensoresCliente);
+        this.router.post('/listarSensoresCliente', Utils.verifyJWT, cache('1 minutes'), sensorController.listarSensoresCliente);
         this.router.post('/alterarSensorApelido', Utils.verifyJWT, sensorController.alterarSensorApelido);
 
         // Listar consumo
-        this.router.post('/listarConsumoSensor', Utils.verifyJWT, dadosMedicoesController.listarConsumoSensor);
+        this.router.post('/listarConsumoSensor', Utils.verifyJWT, cache('1 minutes'), dadosMedicoesController.listarConsumoSensor);
     }
 }
 
